de19f641b175baf5b3a27d181cf14fd02279f126
Jay Slade <brandonslade2@gmail.com>
2022-07-18 20:28:39 -0700

    findLuckyNumbers finish

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,1 +1,20 @@
-// Write your code below this line.
+
+
+
+const luckyNumbers = (n) => {
+    let arr = [1,2,3,4,5,6,7,8,9,10]
+    let newarr = []
+    for (i = 0; i < n; i++) {
+        let x = Math.floor(Math.random()*arr.length)
+        newarr.push(arr[x])
+        arr.splice(x,1)
+    }
+return newarr
+    
+}
+
+console.log(luckyNumbers(5))
+
+
+//learned here that math.random also would need math.floor. and it can be as simple as pushing 
+
